688443d85b30ccfea9dfca598d31e4e84736d900
Michael Dodwell <1372930+dodwmd@users.noreply.github.com>
2025-05-18 22:15:05 +1000

    chore: add ESLint configuration and fix TypeScript types

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    // Add any custom rules here
+    'no-console': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+  ignorePatterns: ['dist/**/*', 'node_modules/**/*', '**/*.d.ts'],
+};
